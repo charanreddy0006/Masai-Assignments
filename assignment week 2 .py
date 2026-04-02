0ed53ea51cd384f4bfd3ed5c5c4e8f768653b81d
Mukka Charan Kumar Reddy <charanreddym2007@gmail.com>
2026-04-02 20:21:27 +0530

    Initial commit

---
+++ assignment week 2 .py
@@ -0,0 +1,11 @@
+name = "Charan Kumar Reddy"
+age = 19
+height = 1.79832
+is_student = True
+print (name , age , height , is_student )
+print(f"Name:{name}|Age:{age}|Height:{height}|Student:{is_student}")
+
+print("Age in Months :",age*12)
+print("Age in days:", age*365)
+print("The remainder when age is divided by 7:", age/7)
+print("Age raised to the power of 2 :", age*age)
